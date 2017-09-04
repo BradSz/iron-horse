ca12e1fbee5c6f834276cdeebf538efac8733c31
Brad Sz <BradSz@outlook.com>
2017-09-03 23:03:22 -0400

    Added additional standard includes

--- src/iron/core.h
+++ src/iron/core.h
@@ -1,12 +1,17 @@
 #pragma once
 
+// STD and STL
 #include <chrono>
 #include <climits>
 #include <iomanip>
+#include <iostream>
+#include <list>
 #include <map>
 #include <memory>
 #include <queue>
+#include <stdint.h>
 #include <sstream>
+#include <vector>
 
 // Threading
 #include <condition_variable>
