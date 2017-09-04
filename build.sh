22452a93daa4dc2bd711fa347d1d9307b31652a0
Brad Sz <BradSz@outlook.com>
2017-09-03 21:16:14 -0400

    Updated build script to use correct directory

--- build.sh
+++ build.sh
@@ -2,24 +2,16 @@
 
 set -e
 
-# root_dir="$(readlink -f "$(dirname "$0")")"
-#build_dir="$root_dir/staging"
-#  bin_dir="$root_dir/bin"
-#
-#mkdir --parents "$build_dir"
-#mkdir --parents "$bin_dir"
-#
-#cd "$build_dir"
-#cmake "$root_dir"
-#cmake --build "$build_dir"
-
-
 dir="$(readlink -f "$(dirname "$0")")"
 
 cd "$dir"
 mkdir -p bin build
 
-cd build
+# Build Makefile(s)
+cd "$dir/build"
 cmake ..
+
+# Execute Makefile(s)
+cd "$dir"
 make -j$(nproc --ignore=1)
 
