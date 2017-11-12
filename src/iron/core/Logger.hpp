f971e835854781e1a8ea96a1728f90f7e67251ef
Brad Sz <BradSz@outlook.com>
2017-11-11 22:14:00 -0500

    Updated logger to include function
    Now restricts usage to compilers that support that preprocessor macro

--- src/iron/core/Logger.hpp
+++ src/iron/core/Logger.hpp
@@ -4,11 +4,11 @@
 
 #ifndef IRON_LOGGER
   #define IRON_LOGGER
-  #define LOG_BASE(file, line, level, msg) (std::cout << level << " " << file << ":" << line << " - " << msg << std::endl)
+  #define LOG_BASE(file, line, context, level, msg) (std::cout << level << " " << file << ":" << line << " " << context << " - " << msg << std::endl)
 
-  #define LOG_TRACE(msg) (LOG_BASE(__FILE__, __LINE__, "TRACE", msg))
-  #define LOG_DEBUG(msg) (LOG_BASE(__FILE__, __LINE__, "DEBUG", msg))
-  #define LOG_INFO (msg) (LOG_BASE(__FILE__, __LINE__, "INFO ", msg))
-  #define LOG_ERROR(msg) (LOG_BASE(__FILE__, __LINE__, "ERROR", msg))
-  #define LOG_FATAL(msg) (LOG_BASE(__FILE__, __LINE__, "FATAL", msg))
+  #define LOG_TRACE(msg) (LOG_BASE(__FILE__, __LINE__, __func__, "TRACE", msg))
+  #define LOG_DEBUG(msg) (LOG_BASE(__FILE__, __LINE__, __func__, "DEBUG", msg))
+  #define LOG_INFO (msg) (LOG_BASE(__FILE__, __LINE__, __func__, "INFO ", msg))
+  #define LOG_ERROR(msg) (LOG_BASE(__FILE__, __LINE__, __func__, "ERROR", msg))
+  #define LOG_FATAL(msg) (LOG_BASE(__FILE__, __LINE__, __func__, "FATAL", msg))
 #endif
