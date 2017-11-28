2e6640690c2b322c76808d59248a62257bd88f83
Brad Sz <BradSz@outlook.com>
2017-11-27 23:19:03 -0500

    Updated build.sh to build in root directory.
    Previously tried to build in {root}/build directory, which
    is no longer necessary since the objects are being built
    within the CMakeFiles directory.

--- build.sh
+++ build.sh
@@ -5,13 +5,10 @@ set -e
 dir="$(readlink -f "$(dirname "$0")")"
 
 cd "$dir"
-mkdir -p bin build
 
 # Build Makefile(s)
-cd "$dir/build"
 cmake "$dir"
 
 # Execute Makefile(s)
-cd "$dir"
 make -j$(nproc --ignore=1)
 
