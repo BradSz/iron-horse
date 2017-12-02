ac37780fed467f66353187f733413d0364eb02ce
Brad Sz <BradSz@outlook.com>
2017-12-02 16:15:01 -0500

    Updated TQueue mutex paradigm
    Updated to have a shared-read/unique-write paradigm

--- src/iron/core/TQueue.hpp
+++ src/iron/core/TQueue.hpp
@@ -4,17 +4,22 @@
 
 template <typename T>
 class TQueue {
+  typedef std::shared_mutex Mutex;
+  typedef std::shared_lock<Mutex> ReadLock;
+  typedef std::unique_lock<Mutex> WriteLock;
 public:
   TQueue(uint64_t maxSize=std::numeric_limits<uint64_t>::max())
-    : MaxSize(maxSize), mPushedNotifier(), mPoppedNotifier(), mMutex(), mQueue() {}
+    : MaxSize(maxSize), mQueue(), mMutex(), mPushedNotifier(), mPoppedNotifier() {}
 
   virtual ~TQueue() {}
 
+  bool push_back(const T& t, uint64_t inTimeoutMs=std::numeric_limits<uint64_t>::max()) {
+    WriteLock padlock(mMutex);
 
-  bool push_back(const T& t, uint64_t timeout=std::numeric_limits<uint64_t>::max()) {
-    std::unique_lock<std::mutex> padlock(mMutex);
+    auto timeout       = std::chrono::milliseconds(inTimeoutMs);
+    auto checkHasSpace = [this] { return mQueue.size() < MaxSize; };
 
-    if (mPoppedNotifier.wait_for(padlock, std::chrono::milliseconds(timeout), [this] { return mQueue.size() < MaxSize; })) {
+    if (mPoppedNotifier.wait_for(padlock, timeout, checkHasSpace)) {
       mQueue.push_back(t);
 
       padlock.unlock();
@@ -24,10 +29,13 @@ public:
     else { return false; }
   }
 
-  bool pop_front (T& t, uint64_t timeout=std::numeric_limits<uint64_t>::max()) {
-    std::unique_lock<std::mutex> padlock(mMutex);
+  bool pop_front (T& t, uint64_t inTimeoutMs=std::numeric_limits<uint64_t>::max()) {
+    WriteLock padlock(mMutex);
 
-    if (mPushedNotifier.wait_for(padlock, std::chrono::milliseconds(timeout), [this] { return !mQueue.empty(); })) {
+    auto timeout      = std::chrono::milliseconds(inTimeoutMs);
+    auto checkHasItem = [this] { return !mQueue.empty(); };
+
+    if (mPushedNotifier.wait_for(padlock, timeout, checkHasItem)) {
       t = mQueue.front();
       mQueue.pop_front();
 
@@ -38,14 +46,14 @@ public:
     else { return false; }
   }
 
-  bool empty() { return mQueue.empty(); }
-  bool size()  { return mQueue.size();  }
+  bool empty() const { ReadLock padlock(mMutex); return mQueue.empty(); }
+  bool size()  const { ReadLock padlock(mMutex); return mQueue.size();  }
 
   uint64_t MaxSize;
 
 private:
+  std::deque<T>           mQueue;
+  mutable std::mutex      mMutex;
   std::condition_variable mPushedNotifier;
   std::condition_variable mPoppedNotifier;
-  std::mutex              mMutex;
-  std::deque<T>           mQueue;
 };
