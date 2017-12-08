a4f98e7b8185cfeb8bcfe535d08458a1ae581fec
Brad Sz <BradSz@outlook.com>
2017-12-07 23:46:41 -0500

    Updated CMakeLists to create BOOST unit tests

---
+++ test/iron/math/PrimeCalculatorTest.cpp
@@ -0,0 +1,13 @@
+#define BOOST_TEST_MODULE boxed
+#include <boost/test/included/unit_test.hpp>
+//#include <src/iron-horse/threading/Boxed.hpp>
+
+BOOST_AUTO_TEST_SUITE(boxed)
+
+BOOST_AUTO_TEST_CASE(t1)
+{
+  BOOST_CHECK(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
