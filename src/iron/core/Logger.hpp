c27f0f02d82b7032aa3f02b61101505e4b49c3dc
Brad Sz <BradSz@outlook.com>
2018-01-27 21:02:59 -0500

    Updates logger to use boost_log backend
    Linker update to link log. Macros updated to wrap BOOST_LOG_TRIVIAL macros

--- src/iron/core/Logger.hpp
+++ src/iron/core/Logger.hpp
@@ -1,14 +1,16 @@
 #pragma once
 
+#define BOOST_LOG_DYN_LINK
+#include <boost/log/trivial.hpp>
+
 // Logging macros...for now everything is logged to console -- TBR (tons)
 
 #ifndef IRON_LOGGER
   #define IRON_LOGGER
-  #define LOG_BASE(file, line, context, level, msg) (std::cout << std::this_thread::get_id() << ":" << level << " " << file << ":" << line << " " << context << " - " << msg << std::endl)
-
-  #define LOG_TRACE(msg) (LOG_BASE(__FILE__, __LINE__, __func__, "TRACE", msg))
-  #define LOG_DEBUG(msg) (LOG_BASE(__FILE__, __LINE__, __func__, "DEBUG", msg))
-  #define LOG_INFO(msg)  (LOG_BASE(__FILE__, __LINE__, __func__, "INFO ", msg))
-  #define LOG_ERROR(msg) (LOG_BASE(__FILE__, __LINE__, __func__, "ERROR", msg))
-  #define LOG_FATAL(msg) (LOG_BASE(__FILE__, __LINE__, __func__, "FATAL", msg))
+  #define LOG_TRACE(msg) BOOST_LOG_TRIVIAL(trace)   << msg;
+  #define LOG_DEBUG(msg) BOOST_LOG_TRIVIAL(debug)   << msg;
+  #define LOG_INFO(msg)  BOOST_LOG_TRIVIAL(info)    << msg;
+  #define LOG_WARN(msg)  BOOST_LOG_TRIVIAL(warning) << msg;
+  #define LOG_ERROR(msg) BOOST_LOG_TRIVIAL(error)   << msg;
+  #define LOG_FATAL(msg) BOOST_LOG_TRIVIAL(fatal)   << msg;
 #endif
